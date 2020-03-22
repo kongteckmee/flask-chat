b6f4ab46de396a7a9746d6c3a04e0b20cee90fde
Teck Mee_May <kongteckmee@gmail.com>
2020-03-22 11:16:45 +0000

    Adding routing

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
+def add_messages(username, message):
+    messages.append("{}: {}".format(username, message))
 
 
 @app.route("/")
@@ -12,12 +15,15 @@ def index():
 
 @app.route("/<username>")
 def user(username):
-    return "Hi " + username
+    """Display chat messages"""
+    return "Welcome, {0} - {1}".format(username, messages)
 
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
